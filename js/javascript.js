6792ef99f0046720682afcd25e1bf10cfbed144e
GurarpitSingh <74349690+GurarpitSingh@users.noreply.github.com>
2021-05-25 17:55:29 +0530

    UnitedAir Project

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+var loader = document.querySelector(".loader")
+
+window.addEventListener("load", vanish);
+
+
+function vanish() {
+    loader.classList.add("disappear");
+}
+
+
+
+
+
+
+// smooth scroll
+
+
+
+
+
+
+
+// smooth scroll end
